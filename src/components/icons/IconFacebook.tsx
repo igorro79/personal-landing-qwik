ca1f32e36b45d42f0105641ac1754b246cde9b70
igorro79 <griniov.igor@gmail.com>
2023-07-06 21:38:28 +0300

    icons qcomponents

--- src/components/icons/IconFacebook.tsx
+++ src/components/icons/IconFacebook.tsx
@@ -1,7 +1,15 @@
-export const IconFacebook = () => (
- <svg
-                    class="h-[20px] w-[20px] lg:h-[22px] lg:w-[23px] xl:h-[23px] xl:w-[23px] "
-                    fill="currentColor"
-                    xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512">
-                    <path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z" /></svg>
-);
+import { component$ } from "@builder.io/qwik";
+
+export const IconFacebook = component$(() => {
+  return (
+    <svg
+      class="h-[20px] w-[20px] lg:h-[22px] lg:w-[23px] xl:h-[23px] xl:w-[23px] "
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+      height="1em"
+      viewBox="0 0 320 512"
+    >
+      <path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z" />
+    </svg>
+  );
+});
